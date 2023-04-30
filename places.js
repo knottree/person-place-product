90c9a271313d393684f755c692e32bdc834cf4a8
knottree <125846114+knottree@users.noreply.github.com>
2023-04-30 16:27:43 -0400

    added a bunch of persons and places

--- places.js
+++ places.js
@@ -3,6 +3,18 @@ const placeArray = [
     {"name":"Burns United Methodist Church", "URL":"Burns_United_Methodist_Church"},
     {"name":"Antioch Missionary Baptist Church", "URL":"Antioch_Missionary_Baptist_Church"},
     {"name":"John Brown Farm State Historic Site", "URL":"John_Brown_Farm_State_Historic_Site"},
-    {"name":"Harpers Ferry National Historical Park", "URL":"Harpers_Ferry_National_Historical_Park"}
+    {"name":"Harpers Ferry National Historical Park", "URL":"Harpers_Ferry_National_Historical_Park"},
+    {"name":"Lawrence Experiment Station", "URL":"Lawrence_Experiment_Station"},
+    {"name":"National Cancer Center", "URL":"National_Cancer_Center"},
+    {"name":"National Institute of Population and Social Security Research", "URL":"National_Institute_of_Population_and_Social_Security_Research"},
+    {"name":"Geospatial Information Authority of Japan", "URL":"Geospatial_Information_Authority_of_Japan"},
+    {"name":"Center for International Forestry Research", "URL":"Center_for_International_Forestry_Research"},
+    {"name":"Center for Tropical Forest Science", "URL":"Center_for_Tropical_Forest_Science"},
+    {"name":"European Forest Institute", "URL":"European_Forest_Institute"},
+    {"name":"International Union of Forest Research Organizations", "URL":"International_Union_of_Forest_Research_Organizations"},
+    {"name":"Forestry Research Institute of Nigeria", "URL":"Forestry_Research_Institute_of_Nigeria"},
+    {"name":"Forestry and Agricultural Biotechnology Institute", "URL":"Forestry_and_Agricultural_Biotechnology_Institute"},
+    {"name":"International Livestock Research Institute", "URL":"International_Livestock_Research_Institute"},
+    {"name":"Applied Physics Laboratory", "URL":"Applied_Physics_Laboratory"}
 ];
 export default placeArray;
